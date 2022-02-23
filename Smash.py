85a4be4d1e6187061b87d8b5420d915719fdc351
Lazik <lazizsaidov95@gmail.com>
2022-02-24 00:34:02 +0500

    initial

---
+++ Smash.py
@@ -0,0 +1,5 @@
+print('Здравствуйте вас приветствует компания AviaSale')
+departure=input('Укажите город вылета: ')
+arrival=input('Укажите город прилёта: ')
+print('Вы выбрали напрвление')
+print(departure,' - ',arrival)
