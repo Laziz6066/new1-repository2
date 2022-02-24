fb8b4a94a8e95687a5d7be3f5da54cc251e14590
Lazik <lazizsaidov95@gmail.com>
2022-02-24 23:40:14 +0500

    wer

--- Smash.py
+++ Smash.py
@@ -3,7 +3,4 @@ departure=input('Укажите город вылета: ')
 arrival=input('Укажите город прилёта: ')
 print('Вы выбрали напрвление')
 print(departure,' - ',arrival)
-print('В городе карши')
-
-print('Я живу в мирное время')
 
