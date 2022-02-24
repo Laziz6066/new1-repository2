55fbea105e71e5d8958d2c97d1a52c40a7ff3796
Lazik <lazizsaidov95@gmail.com>
2022-02-24 23:32:30 +0500

    knop

--- Smash.py
+++ Smash.py
@@ -5,16 +5,3 @@ print('Вы выбрали напрвление')
 print(departure,' - ',arrival)
 
 
-print('Это копия репозитория')
-print('************************')
-
-print('Здравствуйте вас приветствует компания AviaSale')
-departure=input('Укажите город вылета: ')
-arrival=input('Укажите город прилёта: ')
-print('Вы выбрали напрвление')
-
-
-print('Это копия репозитория')
-print('************************')
-
-print('qwekkwekqwmelkmdfgkdfmgkmfglkmdfglkmfdglkmdfg')
